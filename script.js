c3ea0290cadb74e85103ed76de2a49e8ab80d197
Jeremias Heredia <jeremiasheredia07@gmail.com>
2024-12-08 23:03:55 -0300

    Calculadora Basica con funcion memoria de un solo dato.

---
+++ script.js
@@ -0,0 +1,34 @@
+//Esto agrega a la pantalla el valor del boton de la calculadora que toquemos
+function addcheck(valor) {
+
+    document.getElementById('pantalla').value += valor
+
+}
+
+
+function borrar() {
+
+    document.getElementById('pantalla').value = ''
+}
+
+function calcular() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = eval(valorPantalla);
+    document.getElementById('pantalla').value = resultado;
+    
+    
+}
+
+function memoria() {
+
+    document.getElementById('pantallaParaMemoria').value = document.getElementById('pantalla').value
+}
+
+function usarDatomemoria () {
+    document.getElementById('pantalla').value += valor  = document.getElementById('pantallaParaMemoria').value;
+    document.getElementById('pantallaParaMemoria').value = '';
+    
+}
+
+
+
